05bac58f5e8fed721971bf9d04b2869437d12d37
Yodsaran Panomratanarak <70568348+yodsaran02@users.noreply.github.com>
2026-02-25 02:46:21 +0000

    fix: update task creation and editing logic, add UUID generation

--- lib/storage.py
+++ lib/storage.py
@@ -1,4 +1,5 @@
 import json
+import uuid 
 
 def getAllTask():
     with open("task.json", mode="r", encoding="utf-8") as read_file:
@@ -9,11 +10,16 @@ def writeToJson(data):
     with open("task.json", mode="w", encoding="utf-8") as read_file:
         json.dump(data, read_file)
 
-def createTask(title, duedate=None): 
-    tasks = getAllTask()
+def parseDueDate(duedate):
     if duedate:
         duedate = duedate.toISOString()
+    return duedate
+
+def createTask(title, duedate=None): 
+    tasks = getAllTask()
+    duedate = parseDueDate(duedate)
     currentTask = {
+        "hash": uuid.uuid4().hex
         "title": title,
         "duedate": duedate,
         "tags": [],
@@ -23,9 +29,18 @@ def createTask(title, duedate=None):
     tasks.append(currentTask)
     writeToJson(tasks)
 
-def editTask(title):
+def editTask(hash, title=None, duedate=None, actual_time=0, predicted_time=None, tags=[]):
     tasks = getAllTask()
-
+    for task in tasks:
+        if task["hash"] == hash:
+            task["title"] = title
+            task["duedate"] = parseDueDate(duedate)
+            task["actual_time"] = actual_time
+            task["predicted_time"] = predicted_time
+            task["tags"] = tags
+        else:
+            return "Not found"
+            
 def getAllTaskName():
     tasks = getAllTask()
     return [task["title"] for task in tasks]
