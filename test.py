5e74f74faec4afdc47b232470b008ded525f6f3d
Peepol Bamrungchaokasem <peepol.bam_g35@mwit.ac.th>
2026-02-25 09:38:00 +0700

    refactor task management logic and improve user prompts

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
+from datetime import datetime
+from lib import storage
+
+
 print("Do-It Version 1.0")
 print("Select Menu below")
 print("1. add work")
 print("2. show work")
-print("3. remove finished work")
-print("Q: exit")
-work_list = []
+print("3. finished work")
+print("Q: save&exit")
+work_list = storage.getAllTask()
 
 while True:
     choice = input("Select Menu: ")
@@ -13,16 +17,36 @@ while True:
         break
     if choice == "1":
         input_work = input("Tittle: ")
-        work_list.append(input_work)
+        while True:
+            raw_deadline = input("Duedate (DD/MM/YY), leave blank if no due date: ")
+            if raw_deadline == "":
+                duedate = None
+                break
+            try:
+                duedate = datetime.strptime(raw_deadline, "%d/%m/%y")
+                break
+            except ValueError:
+                print("Invalid date format. Please enter the date in DD/MM/YY format.")
+        work_list.append({"title": input_work, "duedate": duedate})
+        work_list.sort(key=lambda x: (x["duedate"] is None, x["duedate"]))
         print("successfully added", end="\n\n")
     elif choice == "2":
         for i in work_list:
-            print(i)
+            print(i["title"], end="")
+            if i["duedate"] is not None:
+                print(" (Due: {})".format(i["duedate"].strftime("%d/%m/%Y")),)
+            else:
+                print(" (No due date)")
+        print()
     elif choice == "3":
         input_work = input("Input finished work: ")
-        if input_work in work_list:
-            work_list.remove(input_work)
-            print("successfully removed", end="\n\n")
-        else:
+        found = False
+        for i in work_list:
+            if i["title"] == input_work:
+                work_list.remove(i)
+                print("successfully removed", end="\n\n")
+                found = True
+                break
+        if not found:
             print("work not found", end="\n\n")
     else: print("Invalid menu, please select again")
