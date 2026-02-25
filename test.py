c46036f579133e9eed119f05ae16cb04721ba990
Peepol Bamrungchaokasem <peepol.bam_g35@mwit.ac.th>
2026-02-25 09:02:39 +0700

    update menu to English and improve UI

--- test.py
+++ test.py
@@ -1,16 +1,20 @@
-print("โปรแกรมจัดการงาน")
-print("1. เพิ่มงาน")
-print("2. แสดงงานทั้งหมด")
-print("Q: ออกจากโปรแกรม")
+print("Do-It Version 1.0")
+print("Select Menu below")
+print("1. add work")
+print("2. show work")
+print("Q: exit")
 work_list = []
 
-choice = input("เลือกเมนู: ")
-if choice == "1":
-    input_work = input("เพิ่มงาน: ")
-    work_list.append(input_work)
-    print("เพิ่มงานสำเร็จ กด Enter เพื่อกลับสู่เมนูหลัก")
-elif choice == "2":
-    for i in work_list:
-        print(i)
-elif choice == "Q":
-    print("ออกจากโปรแกรม")
+while True:
+    choice = input("Select Menu: ")
+    if choice == "Q" or choice == "q":
+        print("Program is exiting...")
+        break
+    if choice == "1":
+        input_work = input("Tittle: ")
+        work_list.append(input_work)
+        print("successfully added", end="\n\n")
+    elif choice == "2":
+        for i in work_list:
+            print(i)
+    else: print("Invalid menu, please select again")
