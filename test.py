618ac37f09e6a3b93deca38edceeb1a99cdbf615
Peepol Bamrungchaokasem <peepol.bam_g35@mwit.ac.th>
2026-02-25 09:08:44 +0700

    add remove finished work

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ print("Do-It Version 1.0")
 print("Select Menu below")
 print("1. add work")
 print("2. show work")
+print("3. remove finished work")
 print("Q: exit")
 work_list = []
 
@@ -17,4 +18,11 @@ while True:
     elif choice == "2":
         for i in work_list:
             print(i)
+    elif choice == "3":
+        input_work = input("Input finished work: ")
+        if input_work in work_list:
+            work_list.remove(input_work)
+            print("successfully removed", end="\n\n")
+        else:
+            print("work not found", end="\n\n")
     else: print("Invalid menu, please select again")
