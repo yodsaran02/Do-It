19c3c6d487ceca381eb003385330899869ead4a0
Peepol Bamrungchaokasem <peepol.bam_g35@mwit.ac.th>
2026-02-25 08:52:39 +0700

    menu option

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("Hello World")
+print("โปรแกรมจัดการงาน")
+print("1. เพิ่มงาน")
+print("2. แสดงงานทั้งหมด")
+print("Q: ออกจากโปรแกรม")
+work_list = []
+
+choice = input("เลือกเมนู: ")
+if choice == "1":
+    input_work = input("เพิ่มงาน: ")
+    work_list.append(input_work)
+    print("เพิ่มงานสำเร็จ กด Enter เพื่อกลับสู่เมนูหลัก")
+elif choice == "2":
+    for i in work_list:
+        print(i)
+elif choice == "Q":
+    print("ออกจากโปรแกรม")
